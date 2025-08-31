3ceb6168ff12e12d224194dbcf7c38ecdd39273d
RitoTorri <cortezfrancisco025@gmail.com>
2025-08-31 16:06:54 -0400

    Se mejoro la interfaz

--- scripts.js
+++ scripts.js
@@ -1,27 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-   
+document.addEventListener("DOMContentLoaded", function () {
+
     const botones = document.querySelectorAll(".btn");
     const resultado = document.getElementById("Valores");
 
     for (let i = 0; i < botones.length; i++) {
-        botones[i].addEventListener("click", function() {
-            
-            if(resultado.textContent === "ERROR MATH") {
+        botones[i].addEventListener("click", function () {
+
+            if (resultado.textContent === "ERROR MATH") {
                 resultado.textContent = ""
                 resultado.innerHTML += botones[i].value;
-           
-            } else if(botones[i].value === "=") {
-                resultado.textContent = eval(resultado.textContent);
-                if(resultado.textContent === "Infinity") resultado.textContent = "ERROR MATH";
-            }
-            
-            else if(botones[i].value === "reiniciar") resultado.innerHTML = "";
 
-            else if(botones[i].value === "borrar") resultado.innerHTML = resultado.innerHTML.slice(0, -1)
-            
+            } else if (botones[i].value === "=") {
+                resultado.textContent = eval(resultado.textContent);
+                if (resultado.textContent === "Infinity") resultado.textContent = "ERROR MATH";
+            }
+
+            else if (botones[i].value === "reiniciar") resultado.innerHTML = "";
+
+            else if (botones[i].value === "borrar") resultado.innerHTML = resultado.innerHTML.slice(0, -1)
+
             else resultado.innerHTML += botones[i].value;
         })
     }
+
+    document.getElementById("git").addEventListener("click", function () {
+        window.open("https://github.com/RitoTorri", "_blank");
+    })
 })
 
 
